3bd7bbbc082958d80a52ef8255f088414701ed6a
eles <ivaneleskin2019@gmail.com>
2021-09-24 00:53:26 +0300

    add about

--- iva_site/songsapp/urls.py
+++ iva_site/songsapp/urls.py
@@ -4,4 +4,5 @@ from songsapp.views import *
 app_name = 'songs'
 urlpatterns = [
     path('', songs, name='song'),
+    path('about/', about, name='about'),
 ]
--- iva_site/songsapp/views.py
+++ iva_site/songsapp/views.py
@@ -6,11 +6,17 @@ from songsapp.models import Songs, ContentCategory
 MODULE_DIR = os.path.dirname(__file__)
 
 
-
 def songs(request):
-    file_path = os.path.join(MODULE_DIR,'fixtures/product.json')
+    file_path = os.path.join(MODULE_DIR, 'fixtures/product.json')
     contex = {
-        'title': 'Andrey Iva - Songs',
+        'title': 'AndreyIva - ТРЕКИ',
         'songs': Songs.objects.all(),
     }
     return render(request, 'songsapp/songs.html', contex)
+
+
+def about(request):
+    contex = {
+        'title': 'AndreyIva - ОБО МНЕ',
+    }
+    return render(request, 'songsapp/about.html', contex)
