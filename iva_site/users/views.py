4794e4ff63e40e41b3cf9d53278a77799b880734
eles <ivaneleskin2019@gmail.com>
2021-09-23 11:19:56 +0300

    add customizations for login and registration

--- iva_site/users/views.py
+++ iva_site/users/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+from users.forms import UserLoginForm
+
 
 def login(request):
-    contex = {
-        'title':'Andrey Iva - Login'
-    }
-    return render(request, 'users/login.html', contex)
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'title': 'GeekShop - Авторизация', 'form': form}
+    return render(request, 'users/login.html', context)
+
 
 def registration(request):
     contex = {
