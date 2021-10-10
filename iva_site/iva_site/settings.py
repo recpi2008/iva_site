41c4204d4b1ca8ecb5fb1d1379bc052b5def61ac
eles <ivaneleskin2019@gmail.com>
2021-10-10 13:40:43 +0300

    add fill_db

--- iva_site/iva_site/settings.py
+++ iva_site/iva_site/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-j=_gk!5h3266sc@b0t84#+h_r0+f6vrp#kjzq3%un5m8liomyn
 
 from dotenv import load_dotenv
 
-load_dotenv(BASE_DIR / '.env')
+# load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,20 +85,20 @@ WSGI_APPLICATION = 'iva_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'iva',
-#         'USER':'postgres',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iva',
+        'USER':'postgres',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
