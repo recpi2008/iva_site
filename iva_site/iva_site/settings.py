255d0fbea5aa8676af90e267ddb8ca170eff388d
eles <ivaneleskin2019@gmail.com>
2021-10-10 14:37:30 +0300

    fix //

--- iva_site/iva_site/settings.py
+++ iva_site/iva_site/settings.py
@@ -155,7 +155,7 @@ LOGIN_URL = '/users/login/'
 # EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
 
 # DOMAIN_NAME = 'http://localhost:8000'
-DOMAIN_NAME = 'http://89.108.77.161/'
+DOMAIN_NAME = 'http://89.108.77.161'
 EMAIL_HOST = 'smtp.yandex.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'recpi5@yandex.ru'
