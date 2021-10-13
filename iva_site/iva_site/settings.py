a69edfd6294470479942a883e04c59f8708e251b
eles <ivaneleskin2019@gmail.com>
2021-10-13 14:47:04 +0300

    fix basa sql

--- iva_site/iva_site/settings.py
+++ iva_site/iva_site/settings.py
@@ -85,20 +85,20 @@ WSGI_APPLICATION = 'iva_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'iva',
-        'USER':'postgres',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'iva',
+#         'USER':'postgres',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
