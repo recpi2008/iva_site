dc0fcc339572e372f24f94457748a5835b14ff88
eles <ivaneleskin2019@gmail.com>
2021-10-15 16:20:43 +0300

    mysql

--- iva_site/iva_site/settings.py
+++ iva_site/iva_site/settings.py
@@ -28,7 +28,7 @@ from dotenv import load_dotenv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
