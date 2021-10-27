478417e6634a430433b70c264e4abc06ff9ba72f
eles <ivaneleskin2019@gmail.com>
2021-10-27 21:57:56 +0300

    fix

--- iva_site/iva_site/settings.py
+++ iva_site/iva_site/settings.py
@@ -85,22 +85,22 @@ WSGI_APPLICATION = 'iva_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'cq45999_iva',
-#         'USER':'cq45999_iva',
-#         'PASSWORD':'PR3w2w9N',
-#         'HOST':'localhost',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cq45999_iva',
+        'USER':'cq45999_iva',
+        'PASSWORD':'PR3w2w9N',
+        'HOST':'localhost',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
